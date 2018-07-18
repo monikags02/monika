5eca2f7c50d4ba38ee41cff2cb2b4ea44271cf4e
monikags02 <40878529+monikags02@users.noreply.github.com>
2018-07-18 13:05:40 +0530

    Create tb.py

---
+++ tb.py
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+#include <stdlib.h>
+
+int main() 
+{
+    long int a[100000],q,i,j,n=0,t; 
+    scanf("%d", &q);
+     for(i = 0; i < q; i++)
+       scanf("%ld",&a[i]);
+	 
+	 for (i = 0; i < q-1; i++)      
+		for (j = 0; j < q-i-1; j++) 
+           if (arr[j] < a[j+1])
+              {	t =a[j+1];
+			  	a[j+1]=a[j];
+			  	a[j]=t;
+			  }
+	for(i = 0; i < q; i++)
+    	n=(n*10)+arr[i];
+    	
+    	printf("%ld",n);
+}
